9efe9a1df81434eccebec158e4244e9f0eddb9ee
AdityaAmbikesh <adityaambikesh@gmail.com>
2018-06-06 11:01:07 +0530

    Push Notifications using Firebase

---
+++ app/src/main/java/com/example/aditya/fcmdemo/MyFirebaseInstanceIDService.java
@@ -0,0 +1,26 @@
+package com.example.aditya.fcmdemo;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by aditya on 6/6/18.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+
+        //For registration of token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //To displaying token on logcat
+        Log.d("TOKEN: ", refreshedToken);
+
+    }
+
+
+}
